9ab0c8f69cead1339e9d5e04a9a8fcd58142b53b
dinesh <dineshswamy92@gmail.com>
2013-12-06 15:27:31 +0530

    styles updated

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 var color='#'+Math.floor(Math.random()*16777215).toString(16);
 console.log('"'+color+'"');
 document.body.style.backgroundColor='"'+color+'"';
+document.getElementById("focus").innerHTML="NOPE";
 });
+
+function doThis()
+{
+	console.log("we are here");
+	document.getElementById("focus").innerHTML="NOPE";
+}
+
+
+window.onload=doThis;
